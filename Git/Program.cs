1d1ac54c46cf56c2a101d9689aa061876c38556b
nikita <Shikabatareo@gmail.com>
2023-03-15 17:24:56 +0300

    asdada commit

--- Git/Program.cs
+++ Git/Program.cs
@@ -54,6 +54,8 @@ namespace Git
                     {
                         Console.WriteLine("Ошибка: деление на ноль");
                     }
+
+                    int a;
                     else
                     {
                         Console.WriteLine("Результат: " + ((double)c / d));
